b02a734b5e48bbe690b525028aa73b3c26d7151f
sezaite <martyna.sezaite@gmail.com>
2021-02-25 07:27:24 +0000

    kazkodel veikia

--- astuntas/pink.php
+++ astuntas/pink.php
@@ -1,13 +1,5 @@
 <!-- Sukurkite du puslapius pink.php ir rose.php. Nuspalvinkite juos atitinkamo spalvom. Į pink.php puslapį įdėkite formą su POST metodu ir mygtuku “GO TO ROSE”. Formą nukreipkite, kad ji atsidarinėtų rose.php puslapyje. Padarykite taip, kad surinkus naršyklėje tiesiogiai adresą į rose.php puslapį, naršyklė būtų peradresuojama į pink.php puslapį.  -->
 
-<?php
-// if (!isset($_POST['rose'])){
-//     header("Location: http://localhost/bit/ndWEB7/astuntas/pink.php");
-//     die;
-//   }
-// jei sita atidarysiu, atsidarys infinity redirectai
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
